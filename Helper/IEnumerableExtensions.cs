5d9b8858d006d708dc9e350d198fb25ad0e7b031
muhammadmb <muhammadmagdyje@gmail.com>
2025-01-12 21:47:02 +0200

    Implement ShapeData extension method for IEnumerable and Object

---
+++ Helper/IEnumerableExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using System.Reflection;
+
+namespace EquipmentAPI.Helper
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<ExpandoObject> ShapeData<TSource>(
+            this IEnumerable<TSource> sources,
+            string fields)
+        {
+            if (sources == null)
+            {
+                throw new ArgumentNullException(nameof(sources));
+            }
+
+            var expandoObjectList = new List<ExpandoObject>();
+
+            var propertyInfoList = new List<PropertyInfo>();
+
+            if (string.IsNullOrWhiteSpace(fields))
+            {
+                var propertyInfos = typeof(TSource)
+                    .GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+                propertyInfoList.AddRange(propertyInfos);
+            }
+            else
+            {
+                var fieldsAfterSplit = fields.Split(',');
+
+                foreach (var field in fieldsAfterSplit)
+                {
+                    var propertyName = field.Trim();
+
+                    var propertyInfos = typeof(TSource)
+                        .GetProperty(propertyName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+
+                    if (propertyInfos == null)
+                    {
+                        throw new Exception($"Property {propertyName} wasn't found" + $"{typeof(TSource)}");
+                    }
+                    propertyInfoList.Add(propertyInfos);
+                }
+            }
+            foreach (TSource sourceObject in sources)
+            {
+                var dataShapedObject = new ExpandoObject();
+                foreach (var propertyInfo in propertyInfoList)
+                {
+                    var propertyValue = propertyInfo.GetValue(sourceObject);
+
+                    ((IDictionary<string, object>)dataShapedObject).Add(propertyInfo.Name, propertyValue);
+                }
+                expandoObjectList.Add(dataShapedObject);
+            }
+
+            return expandoObjectList;
+
+        }
+    }
+}
