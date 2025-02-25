8c1e709596c68a92c2a948e0e53b34085003a8d5
muhammadmb <muhammadmagdyje@gmail.com>
2025-02-25 12:56:33 +0200

    Add Equipment Repository

---
+++ Repositories/Equipment Repository/IEquipmentRepository.cs
@@ -0,0 +1,21 @@
+﻿using EquipmentAPI.Entities;
+using EquipmentAPI.Helper;
+using EquipmentAPI.ResourceParameters;
+
+namespace EquipmentAPI.Repositories.Equipment_Repository
+{
+    public interface IEquipmentRepository
+    {
+        Task<PagedList<Equipment>> GetEquipment(EquipmentResourceParameters equipmentResourceParameters);
+
+        Task<Equipment> GetEquipmentById(Guid equipmentId, string fields);
+
+        void Create(Equipment equipment);
+
+        void Delete(Guid equipmentId);
+
+        Task Update(Equipment equipment);
+
+        Task<bool> SaveChangesAsync();
+    }
+}
