e75bec14a58b07a06d4618869310baa806e43c94
muhammadmb <muhammadmagdyje@gmail.com>
2025-11-20 22:50:24 +0200

    Add Supplier resources parameters for filtering

--- EquipmentAPI/ResourceParameters/SupplierResourceParameters.cs
+++ EquipmentAPI/ResourceParameters/SupplierResourceParameters.cs
@@ -2,5 +2,19 @@
 {
     public class SupplierResourceParameters : ResourceParameters
     {
+        private string _FilterByCountry = "";
+        private string _FilterByCity = "";
+
+        public string FilterByCountry
+        {
+            get => _FilterByCountry;
+            set => _FilterByCountry = value?.Trim().ToLowerInvariant();
+        }
+
+        public string FilterByCity
+        {
+            get => _FilterByCity;
+            set => _FilterByCity = value?.Trim().ToLowerInvariant();
+        }
     }
 }
