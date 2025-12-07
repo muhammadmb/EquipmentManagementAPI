b53abbbc43d543db644b4000f3c19cf476383e9e
muhammadmb <muhammadmagdyje@gmail.com>
2025-12-07 23:40:18 +0200

    Add Customer Resource Parameters

--- EquipmentAPI/ResourceParameters/CustomerResourceParameters.cs
+++ EquipmentAPI/ResourceParameters/CustomerResourceParameters.cs
@@ -1,6 +1,20 @@
 ﻿namespace EquipmentAPI.ResourceParameters
 {
-    public class CustomerResourceParameters  : ResourceParameters
+    public class CustomerResourceParameters : ResourceParameters
     {
+        private string _FilteredByCountry = "";
+        private string _FilteredByCity = "";
+
+        public string FilterByCountry
+        {
+            get => _FilteredByCountry;
+            set => _FilteredByCountry = value.Trim().ToLowerInvariant();
+        }
+
+        public string FilterByCity
+        {
+            get => _FilteredByCity;
+            set => _FilteredByCity = value.Trim().ToLowerInvariant();
+        }
     }
 }
