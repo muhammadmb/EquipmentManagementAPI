595b5ad644dac4ce2f49331f9b50157e94824218
muhammadmb <muhammadmagdyje@gmail.com>
2025-12-08 00:22:53 +0200

    Refactor CustomerUpdateDto: remove PhoneNumbers property

--- EquipmentAPI/Models/CustomersModels/Write/CustomerUpdateDto.cs
+++ EquipmentAPI/Models/CustomersModels/Write/CustomerUpdateDto.cs
@@ -1,13 +1,10 @@
-﻿using EquipmentAPI.Entities;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace EquipmentAPI.Models.CustomersModels.Write
 {
     public class CustomerUpdateDto
     {
         public string Name { get; set; } = string.Empty;
-
-        public List<CustomerPhoneNumber> PhoneNumbers { get; set; } = new();
 
         [EmailAddress]
         public string Email { get; set; } = string.Empty;
