a064021acbe5d98415152c1cdfd385144a44d4a5
muhammadmb <muhammadmagdyje@gmail.com>
2026-01-01 01:14:01 +0200

    Add new properties to RentalContract and update namespaces
    Added CancelledDate, FinishedDate, Status, and SuspendedDate columns to the RentalContracts table via EF Core migration. Updated ApplicationDbContextModelSnapshot and migration designer files to reflect these changes. Changed entity mapping namespaces from EquipmentAPI.Entities to Domain.Entities for consistency.

---
+++ Equipment.Infrastructure/Migrations/20251223022139_AddNewPropertiesinRentalContractEntity.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EquipmentAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewPropertiesinRentalContractEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CancelledDate",
+                table: "RentalContracts",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "FinishedDate",
+                table: "RentalContracts",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "RentalContracts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "SuspendedDate",
+                table: "RentalContracts",
+                type: "datetimeoffset",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CancelledDate",
+                table: "RentalContracts");
+
+            migrationBuilder.DropColumn(
+                name: "FinishedDate",
+                table: "RentalContracts");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "RentalContracts");
+
+            migrationBuilder.DropColumn(
+                name: "SuspendedDate",
+                table: "RentalContracts");
+        }
+    }
+}
