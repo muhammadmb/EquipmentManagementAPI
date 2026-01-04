2acbdead598c9b51ed8d5562d658f95ee5a1b474
muhammadmb <muhammadmagdyje@gmail.com>
2026-01-04 05:50:12 +0200

    Add Rental Contract Resolver

---
+++ EquipmentAPI/GraphQL/RentalContract/Resolvers/RentalContractResolvers.cs
@@ -0,0 +1,33 @@
+﻿using Application.Interface.Repositories;
+using Application.Models.CustomersModels.Read;
+using Application.Models.EquipmentModels.Read;
+using Application.Models.RentalContractModels.Read;
+using Mapster;
+
+namespace API.GraphQL.RentalContract.Resolvers
+{
+    public class RentalContractResolvers
+    {
+        public async Task<CustomerDto?> GetCustomerAsync(
+           [Parent] RentalContractDto contract,
+           [Service] ICustomerRepository customerRepo)
+        {
+            if (contract.CustomerId == Guid.Empty)
+                return null;
+
+            return (await customerRepo.GetCustomerById(contract.CustomerId, null))
+                .Adapt<CustomerDto>();
+        }
+
+        public async Task<EquipmentDto?> GetEquipmentAsync(
+            [Parent] RentalContractDto contract,
+            [Service] IEquipmentRepository equipmentRepo)
+        {
+            if (contract.EquipmentId == Guid.Empty)
+                return null;
+
+            return (await equipmentRepo.GetEquipmentById(contract.EquipmentId, null))
+                .Adapt<EquipmentDto>();
+        }
+    }
+}
