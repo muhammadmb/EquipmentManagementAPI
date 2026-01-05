c5a36057b1c3a8c979750fa75266ea1d937c888a
muhammadmb <muhammadmagdyje@gmail.com>
2026-01-05 12:18:31 +0200

    Add Selling contracts Resource parameters

--- Equipment.Application/ResourceParameters/SellingContractResourceParameters.cs
+++ Equipment.Application/ResourceParameters/SellingContractResourceParameters.cs
@@ -2,5 +2,14 @@
 {
     public class SellingContractResourceParameters : ResourceParameters
     {
+        public Guid? CustomerId { get; set; }
+        public Guid? EquipmentId { get; set; }
+
+        public DateTimeOffset? FromDate { get; set; }
+        public DateTimeOffset? ToDate { get; set; }
+
+        public int? Year { get; set; }
+
+        public int? Month { get; set; }
     }
 }
