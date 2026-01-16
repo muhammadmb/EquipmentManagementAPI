d32d257d099ad77c021423bc5f588fd9fcce0586
muhammadmb <muhammadmagdyje@gmail.com>
2026-01-16 17:25:38 +0200

    Feat: Add Selling contract filter input

---
+++ EquipmentAPI/GraphQL/SellingContract/Inputs/SellingContractFilterInput.cs
@@ -0,0 +1,27 @@
+﻿namespace API.GraphQL.SellingContract.Inputs
+{
+    public class SellingContractFilterInput
+    {
+        public int? PageSize = 10;
+
+        public int? pageNumber = 1;
+
+        public string? SearchQuery = "";
+
+        public string? SortBy = "";
+
+        public bool? SortDescending { get; set; } = false;
+
+        public Guid? CustomerId { get; set; }
+
+        public Guid? EquipmentId { get; set; }
+
+        public DateTimeOffset? FromDate { get; set; }
+
+        public DateTimeOffset? ToDate { get; set; }
+
+        public int? Year { get; set; }
+
+        public int? Month { get; set; }
+    }
+}
