15e133bae7b5094afe57e075f01d047cc25dffe8
muhammadmb <muhammadmagdyje@gmail.com>
2026-01-16 17:25:01 +0200

    feat: Add Selling contract resolver

---
+++ EquipmentAPI/GraphQL/SellingContract/Resolver/SellingContractResolver.cs
@@ -0,0 +1,31 @@
+﻿using Application.Interface.Repositories;
+using Application.Models.CustomersModels.Read;
+using Application.Models.EquipmentModels.Read;
+using Application.Models.SellingContract.Read;
+using Mapster;
+
+namespace API.GraphQL.SellingContract.Resolver
+{
+    public class SellingContractResolver
+    {
+        public async Task<CustomerDto?> GetCustomerAsync(
+           [Parent] SellingContractDto contract,
+           [Service] ICustomerRepository customerRepo)
+        {
+            if (contract.CustomerId == Guid.Empty)
+                return null;
+            return (await customerRepo.GetCustomerById(contract.CustomerId, null))
+                .Adapt<CustomerDto>();
+        }
+
+        public async Task<EquipmentDto?> GetEquipmentAsync(
+            [Parent] SellingContractDto contract,
+            [Service] IEquipmentRepository equipmentRepo)
+        {
+            if (contract.EquipmentId == Guid.Empty)
+                return null;
+            return (await equipmentRepo.GetEquipmentById(contract.EquipmentId, null))
+                .Adapt<EquipmentDto>();
+        }
+    }
+}
