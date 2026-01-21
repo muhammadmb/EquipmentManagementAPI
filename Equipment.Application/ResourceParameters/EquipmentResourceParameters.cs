91fa629ae1ddfee251670b8e43f3da89874c48c8
muhammadmb <muhammadmagdyje@gmail.com>
2026-01-21 16:53:09 +0200

    feat: extend EquipmentResourceParameters with filtering options

--- Equipment.Application/ResourceParameters/EquipmentResourceParameters.cs
+++ Equipment.Application/ResourceParameters/EquipmentResourceParameters.cs
@@ -1,4 +1,6 @@
-﻿namespace Application.ResourceParameters
+﻿using Domain.Enums;
+
+namespace Application.ResourceParameters
 {
     public class EquipmentResourceParameters : ResourceParameters
     {
@@ -9,5 +11,16 @@
             get => _SortBy;
             set => _SortBy = value?.Trim().ToLowerInvariant();
         }
+
+        public EquipmentBrand? EquipmentBrand { get; set; }
+        public EquipmentType? EquipmentType { get; set; }
+        public EquipmentStatus? EquipmentStatus { get; set; }
+
+        public bool? IsAvailable { get; set; }
+
+        public DateTimeOffset? PurchaseDateFrom { get; set; }
+        public DateTimeOffset? PurchaseDateTo { get; set; }
+
+        public int? YearOfManufacturers { get; set; }
     }
 }
