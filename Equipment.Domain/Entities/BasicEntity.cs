28b9c2610a511c59c6f7e8a4101d9f404016debd
muhammadmb <muhammadmagdyje@gmail.com>
2026-01-21 16:50:04 +0200

    Standardize soft delete logic in BasicEntity

--- Equipment.Domain/Entities/BasicEntity.cs
+++ Equipment.Domain/Entities/BasicEntity.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities
 {
@@ -10,8 +11,27 @@ namespace Domain.Entities
 
         public DateTimeOffset? DeletedDate { get; set; }
 
+        [NotMapped]
+        public bool IsDeleted => DeletedDate.HasValue;
+
         public DateTimeOffset? UpdateDate { get; set; }
 
         public byte[] RowVersion { get; set; }
+
+        public virtual void SoftDelete(Guid? userId = null)
+        {
+            if (IsDeleted)
+                return;
+
+            DeletedDate = DateTimeOffset.UtcNow;
+        }
+
+        public virtual void Restore(Guid? userId = null)
+        {
+            if (!IsDeleted)
+                return;
+
+            DeletedDate = null;
+        }
     }
 }
