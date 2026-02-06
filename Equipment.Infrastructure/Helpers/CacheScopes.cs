c0341b1846a7674e164acdb6e29c585d59454de8
muhammadmb <muhammadmagdyje@gmail.com>
2026-02-06 16:42:48 +0200

    Add Equipment cache scope constant to CacheScopes
    Added a new constant Equipment to the CacheScopes class to support caching for equipment-related data.

--- Equipment.Infrastructure/Helpers/CacheScopes.cs
+++ Equipment.Infrastructure/Helpers/CacheScopes.cs
@@ -4,6 +4,7 @@
     {
         public const string RentalContracts = "rental-contracts";
         public const string SellingContracts = "selling-contracts";
+        public const string Equipment = "equipment";
     }
 
 }
